82398086722d9a64a84292c2422eb5c31a7ef719
root <root@localhost.localdomain>
2016-05-20 18:28:34 -0700

    1

--- crack.py
+++ crack.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import time
 
 def parse_cmdline_args():
 
@@ -14,8 +15,18 @@ def main (args):
 	dict = args.dict;
 	cap = args.cap;
 	dict_list = os.listdir(dict);
+	j = 0;
+	while j != len(dict_list):
+		if dict_list[j][-3:] != 'txt':
+			del dict_list[j];
+		else:
+			j += 1;
+	print dict_list;
+ 	time.sleep(4);
 	for i in dict_list:
-		os.system("aircrack-ng -w %s %s" % (dict+i,cap));
+		print "Starting %s \n" %(i);
+		os.system("aircrack-ng -w %s %s |grep -i \"KEY FOUND\" > /root/found" % (dict+i,cap));
+		print "Not found in %s\n" % (i);
 	
 if __name__ == '__main__':
     args = parse_cmdline_args()
